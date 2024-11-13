e1b05990291196b39c4912d6cc167483814256f0
JoeMad21 <56982477+JoeMad21@users.noreply.github.com>
2024-11-13 03:49:28 -0500

    Update ipu_support.hpp

--- device_libraries/ipu_support.hpp
+++ device_libraries/ipu_support.hpp
@@ -15,84 +15,140 @@
 #include <poputil/TileMapping.hpp>
 
 enum HARDWARE {IPU, MODEL, CPU};
-enum MAPPING {LINEAR, SET};
-enum LAYERS {INPUT, CONSUMPTION, OUTPUT};
+enum MAPPING {LINEAR, SET, SIM};
+enum STAGE {INPUT, CONSUMPTION, OUTPUT, STAGES};
 enum IO {IN, OUT};
-enum STAGE {IN, COMP, OUT, STAGES};
 enum COMPATSHAPE {TRIANGLEUP, TRIANGLEQR, TRIANGLEDOWN, SQUARE, LINE, CUSTOM};
-enum TEMPLATE{V}
+enum OP::{NOOP, ADD, SUB, MULT, DECOMP}
 enum CODELETS {POPLIN, POPOPS, POPRAND};
 
-struct layer {
-    std::vector<poplar::Tensor> tensors;
-};
-
-struct model {
-    std::vector<layer> layers;
-};
 
 class graphConfig {
-    public:
+    private:
+
+        // Graphcore IPU Types
         poplar::Graph graph;
-        std::vector<poplar::Graph> IN_vgraphs(consumption_cores);
-        std::vector<poplar::Graph> WORK_vgraphs(consumption_cores);
-        std::vector<poplar::Graph> OUT_vgraphs(consumption_cores);
-        poplar::Device device;
-        model originalModel;
-        std::vector<model> myModels;
-
-        long unsigned int dim = 0;
-        long unsigned int streams = 0;
-        long unsigned int compat = 0;
-        long unsigned int hw = 0;
-        long unsigned int map = 0;
-        long unsigned int buf_depth = 0;
-        long unsigned int offset = 0;
-        long unsigned int num_layers = 0;
+        std::vector<poplar::Graph> vgraphs; // 1D array of virtual graphs, our cores essentially
         
-        std::vector< long unsigned int > layer_lens;
-        std::vector< poplar::program::Program > progs;
+        poplar::Device device;
 
-        std::vector< std::vector<poplar::ComputeSet> > cps;
-        std::vector< std::vector< std::vector<poplar::VertexRef>  > > vtx;
-        std::vector< std::vector< std::vector<poplar::DataStream> > > strm;
+        std::vector<std::vector<std::vector<poplar::Tensor>>> tensors; // 3D array of Tensors;
 
+        std::vector<std::vector<poplar::DataStream>> strms; // 2D array of DataStreams;
+
+        std::vector<std::vector<poplar::ComputeSet>> cps; // 2D array of ComputeSets
+
+        std::vector<std::vector<std::vector<poplar::VertexRef>>> vtx; // 3D array of Vertices
+
+        std::vector<std::vector<poplar::program::Program>> progs; // 2D array of Programs 
+
+
+        poplar::Engine engine;
+
+        // Basic Settings
+        long unsigned int num_devices;
+        long unsigned int hardware;
+        long unsigned int map;
+        long unsigned int num_cores;
+        //long unsigned int comPat;
+        //long unsigned int dtype;
+
+        long unsigned int num_inputs;
+        long unsigned int num_outputs;
+        long unsigned int max_cps;
+        long unsigned int max_tensor_layers;
+        long unsigned int max_vtx;
+        std::vector<long unsigned int> input_dims;
+
+        std::vector<long unsigned int> start_tile_idx;
+        std::vector<long unsigned int> num_tiles;
+        std::vector<long unsigned int> num_tile_layers;
+        std::vector<long unsigned int> num_tensors;
+        std::vector<long unsigned int> num_tensor_layers;
+        std::vector<std::vector<long unsigned int>> tile_layer_widths;
+        std::vector<std::vector<long unsigned int>> tensor_layer_widths;
+
+        // Performance Exploration
+        long unsigned int buf_depth;
+
+
+        //
+        getDevice();
+        allocateVirtualGraphs();
+        allocateStreams();
+        allocatePrograms();
     public:
-
-        graphConfig(boost::program_options::variables_map& vm);
-        void getDevice();
-        void buildLayer(int layer_id, int num_tensors);
-        void addComputeSet(int io);
-        void addStream(int num_port, int io);
-        void addVertex();
-        void connectVertex(poplar::Graph& graph, std::vector<poplar::VertexRef>& vtx, std::vector<model>& myModels, int num_streams, int top_layer, int bottom_layer, int top_tensor, int bottom_tensor, std::string in, std::string out);
-        void connectEngineStream(poplar::Graph& graph, graphConfig& myGraphConfig, int num_port, int IO);
-        void buildTensorTemplate(poplar::Graph& graph, graphConfig& myGraphConfig, int mode, int hw);
-        void buildIOTemplate(poplar::Graph& graph, std::vector<model>& myModels, comPattern& comPat, std::pair<int,int> params, int num_streams, int mode);
+        graphConfig();
+        void buildLayers(long unsigned int& core, long unsigned int& layer_id, long unsigned int& num_tensors, long unsigned int& dim, std::vector<long unsigned int>& mappings = {});
+        void buildLayers(long unsigned int& core, long unsigned int& layer_id, long unsigned int& num_tensors, std::vector<long unsigned int>& dims, std::vector<long unsigned int>& mappings = {});
+        void plotVertex(long unsigned int& core, long unsigned int& layer_id, long unsigned int& idx, long unsigned int& cps_id, long unsigned int& input, long unsigned int& output, long unsigned int& tile_id, std::string& vertexName);
 
 }
 
-//void buildLayer(poplar::Graph& graph, model& myModel, std::pair<int,int> params, int layer_id, int map, int num_tensors);
-//void buildLayer(graphConfig& myGraphConfig, int layer_id, int num_tensors);
-//void appendLayer(graphConfig& myGraphConfig, int layer_id, int num_tensors);
+    // Input Settings
+    // long unsigned int num_inputs;
+    // long unsigned int num_input_tiles;
+    // long unsigned int num_input_layers;
+    // long unsigned int num_input_cores;
+    // long unsigned int input_tile_idx;
+    // std::vector<long unsigned int> input_dims;
 
-//void addComputeSet(poplar::Graph& graph, std::vector<poplar::ComputeSet>& cps, int num_streams, int IO);
-//void addComputeSet(graphConfig& myGraphConfig, int io);
+    // Consumption Settings
+    // long unsigned int num_consumption_tiles;
+    // long unsigned int num_consumption_layers;
+    // long unsigned int num_consumption_cores;
+    // long unsigned int consumption_tile_idx;
+    // std::vector<long unsigned int> consumption_dims;
 
-//void addStream(poplar::Graph& graph, std::vector<poplar::DataStream>& strm, std::pair<int,int> params, int buf_depth, int num_port, int num_streams, int io);
-//void addStream(graphConfig& myGraphConfig, int num_port, int io);
+    // Output Settings
+    // long unsigned int num_outputs;
+    // long unsigned int num_output_tiles;
+    // long unsigned int num_output_layers;
+    // long unsigned int num_output_cores;
+    // long unsigned int output_tile_idx;
+    // std::vector<long unsigned int> output_dims;
 
-//void addVertex(poplar::Graph& graph, std::vector<poplar::ComputeSet>& cps, std::vector<poplar::VertexRef>& vtx, int num_streams, int offset, int hw);
-//void addVertex(graphConfig& myGraphConfig);
+    // poplar::Graph graph;
+    // std::vector<poplar::Graph> IN_vgraphs(consumption_cores);
+    // std::vector<poplar::Graph> WORK_vgraphs(consumption_cores);
+    // std::vector<poplar::Graph> OUT_vgraphs(consumption_cores);
+    // poplar::Device device;
+    // model originalModel;
+    // std::vector<model> myModels;
 
-//void connectVertex(poplar::Graph& graph, std::vector<poplar::VertexRef>& vtx, std::vector<model>& myModels, int num_streams, int top_layer, int bottom_layer, int top_tensor, int bottom_tensor, std::string in, std::string out);
-//void connectVertex(poplar::Graph& graph, std::vector<poplar::VertexRef>& vtx, std::vector<model>& myModels, int num_streams, int top_layer, int bottom_layer, int top_tensor, int bottom_tensor, std::string in, std::string out);
+    // long unsigned int dim = 0;
+    // long unsigned int streams = 0;
+    // long unsigned int compat = 0;
+    // long unsigned int hw = 0;
+    // long unsigned int map = 0;
+    // long unsigned int buf_depth = 0;
+    // long unsigned int offset = 0;
+    // long unsigned int num_layers = 0;
+        
+    // std::vector< long unsigned int > layer_lens;
+    // std::vector< poplar::program::Program > progs;
 
-//void connectEngineStream(poplar::Graph& graph, std::vector<float>& cpu, int num_streams, int num_port, int IO);
-//void connectEngineStream(poplar::Graph& graph, graphConfig& myGraphConfig, int num_port, int IO);
+    // std::vector< std::vector<poplar::ComputeSet> > cps;
+    // std::vector< std::vector< std::vector<poplar::VertexRef>  > > vtx;
+    // std::vector< std::vector< std::vector<poplar::DataStream> > > strm;
 
-//void buildTensorTemplate(poplar::Graph& graph, std::vector<model>& myModels, std::pair<int,int> params, int num_streams, int mode, int hw);
-//void buildTensorTemplate(poplar::Graph& graph, graphConfig& myGraphConfig, int mode, int hw);
-
-//void buildIOTemplate(poplar::Graph& graph, std::vector<model>& myModels, comPattern& comPat, std::pair<int,int> params, int num_streams, int mode);
-//void buildIOTemplate(poplar::Graph& graph, std::vector<model>& myModels, comPattern& comPat, std::pair<int,int> params, int num_streams, int mode);
+    // this->v1 = 0;
+    // this->v2 = 0;
+    // this->v3 = 0;
+    // this->v4 = 0;
+    // this->v5 = 0;
+    // this->v6 = 0;
+    // this->v7 = 0;
+    // this->v8 = 0;
+    // this->hardware = 2;
+    // this->compat = 0;
+    // this->map = 0;
+    // this->buf_depth = 0;
+    // this->packets = 0;
+    // this->consumption_cores = 0;
+    // this->devices = 0;
+    // this->seed = 0;
+    // this->offset = 0;
+    // this->num_layers = 0;
+    // this->num_inputs = 0;
+    // this->num_outputs = 0;
